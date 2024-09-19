985bb1054b529dbaa62b49ef2f6710352c3277ee
Henry Li <henry@hootenanny.io>
2024-09-19 15:00:49 +0100

    rewrite script

--- persist-utm.js
+++ persist-utm.js
@@ -1,60 +1,44 @@
-console.log("Persisting UTM parameters...");
 (function () {
-  // Function to get URL parameters
-  function getUrlParams() {
-    var params = {};
-    var queryString = window.location.search.substring(1);
-    var pairs = queryString.split("&");
-    for (var i = 0; i < pairs.length; i++) {
-      var pair = pairs[i].split("=");
-      params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-    }
-    return params;
-  }
+  console.log("Persisting UTM parameters...");
 
-  // Function to set URL parameters
-  function setUrlParams(params) {
-    var baseUrl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    var newUrl =
-      baseUrl +
-      "?" +
-      Object.keys(params)
-        .map(function (key) {
-          return (
-            encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
-          );
-        })
-        .join("&");
-    window.history.replaceState({ path: newUrl }, "", newUrl);
-  }
-
-  // Get current URL parameters
-  var params = getUrlParams();
-
-  // Check for UTM parameters and persist them if they exist
-  var utmParams = [
+  const STORAGE_KEY = "utmParams";
+  const UTM_PARAMETER_KEYS = [
     "utm_source",
     "utm_medium",
     "utm_campaign",
     "utm_term",
     "utm_content",
   ];
-  var hasUtmParams = utmParams.some(function (param) {
-    return params[param];
+
+  const searchParams = [];
+  new URL(window.location.href).searchParams.forEach((value, key) => {
+    searchParams.push([key, value]);
   });
 
-  if (hasUtmParams) {
-    sessionStorage.setItem("utmParams", JSON.stringify(params));
-  } else {
-    var storedParams = sessionStorage.getItem("utmParams");
-    if (storedParams) {
-      var parsedParams = JSON.parse(storedParams);
-      Object.assign(params, parsedParams);
-      setUrlParams(params);
+  const utmParams = searchParams.filter(([key]) =>
+    UTM_PARAMETER_KEYS.includes(key)
+  );
+
+  const restoreUtmParams = () => {
+    const res = sessionStorage.getItem(STORAGE_KEY);
+    if (!res) {
+      return;
     }
+
+    const utmParams = JSON.parse(res);
+    const url = [
+      new URL(
+        `${window.location.protocol}//${window.location.host}${window.location.pathname}`
+      ),
+      new URLSearchParams(utmParams).toString(),
+    ].join("?");
+
+    window.history.replaceState({ path: url.toString() }, "", url.toString());
+  };
+
+  if (utmParams.length) {
+    sessionStorage.setItem(STORAGE_KEY, JSON.stringify(utmParams));
+  } else {
+    restoreUtmParams();
   }
 })();
